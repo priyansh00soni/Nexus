bfcd329cbde5e5e60affbbef9df7553a637bf4df
Priyansh <priyansh00soni@gmail.com>
2026-06-20 16:19:51 +0530

    feat: refactor server initialization and add graceful shutdown handling

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,22 @@
 import app from "./app.js";
-import connectDB from "./config/db.js";
-import connectRedis from "./config/redis.js";
+import connectDB, { pool } from "./config/db.js";
+import connectRedis, { redis } from "./config/redis.js";
 import logger from "./utils/logger.js";
 
 await connectDB()
 await connectRedis()
 
-app.listen(process.env.APP_PORT || 8000 , ()=>{
+const server = app.listen(process.env.APP_PORT || 8000 , ()=>{
     logger.info(`App listens at port: ${process.env.APP_PORT}`);
 })
+
+const shutdown = async () => {
+    logger.info("Shutting Down Gracefully!")
+    await new Promise((resolve) => server.close(resolve))
+    await pool.end()
+    await redis.quit()
+    process.exit(0)
+}
+
+process.on('SIGTERM', shutdown)
+process.on('SIGINT', shutdown)
