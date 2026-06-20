d55a4990b94dc9529d752cdfdd448babbbdb989c
Priyansh <priyansh00soni@gmail.com>
2026-06-20 12:57:20 +0530

    feat: add cookie-parser and cors types, enhance app configuration and error handling

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,37 @@
-import type { Express } from "express";
+import type { Express, NextFunction, Request, Response } from "express";
 import express from "express";
-
+import cors from 'cors'
+import cookieParser from "cookie-parser";
+import ApiResponse from "./utils/ApiResponse.js";
+import ApiError from "./utils/ApiError.js";
 const app:Express = express()
 
 app.use(
     cors({
         origin:process.env.CORS_ORIGIN,
-        Credentials:true
+        credentials:true
     })
 )
+
+app.use(express.json({limit: '16kb'}))
+
+app.use(express.urlencoded({ extended:true, limit:'16kb'}))
+
+app.use(express.static('public'))
+
+app.use(cookieParser())
+
+//Routes
+
+import HealthRouter from './monitoring/monitoring.routes.js'
+
+app.use('/api/v1/health',HealthRouter)
+
+app.use((err:ApiError | Error,req:Request,res:Response,next:NextFunction)=>{
+    const statusCode = err instanceof ApiError ? err.statusCode : 500
+    return res.status(statusCode || 500).json(
+        new ApiResponse(statusCode || 500,err.message||"Something went wrong")
+    )
+})
 
 export default app
