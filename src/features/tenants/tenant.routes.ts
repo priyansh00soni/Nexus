4bb5751f6f6deb15addd32c36f11f257601ba051
Priyansh <priyansh00soni@gmail.com>
2026-06-23 18:23:15 +0530

    feat: add validation to tenant creation route and update auth middleware to include Response type

--- src/features/tenants/tenant.routes.ts
+++ src/features/tenants/tenant.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import createTenantController from "./tenant.controller.js";
+import validate from "../../middleware/validate.middleware.js";
+import { createTenantSchema } from "./tenant.zodSchema.js";
 
 const router = Router()
 
-router.route('/').post(createTenantController)
+router.route('/').post(validate(createTenantSchema),createTenantController)
 
 export default router
--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,10 +1,10 @@
-import type { NextFunction, Request } from "express";
+import type { NextFunction, Request, Response } from "express";
 import asyncHandler from "../utils/asyncHandler.js";
 import ApiError from "../utils/ApiError.js";
 import { prisma } from "../config/PrismaClient.js";
 import hashApiKey from "../utils/hashApiKey.js";
 
-const authMid = asyncHandler(async(req:Request,next:NextFunction)=>{
+const authMid = asyncHandler(async(req:Request,res:Response,next:NextFunction)=>{
     const apiKey = req.headers['x-api-key'] as string
     if(!apiKey) throw new ApiError(400,"Api key not provided.")
 
