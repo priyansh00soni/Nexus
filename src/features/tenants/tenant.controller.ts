e5a1790c2fe8355f6e1321efe993063cd0071d39
Priyansh <priyansh00soni@gmail.com>
2026-06-23 18:30:16 +0530

    feat: add validation for tenant name in createTenantController

--- src/features/tenants/tenant.controller.ts
+++ src/features/tenants/tenant.controller.ts
@@ -6,7 +6,6 @@ import ApiResponse from "../../utils/ApiResponse.js";
 
 const createTenantController = asyncHandler(async(req: Request, res: Response)=>{
     const {name} = req.body 
-    if(!name) throw new ApiError(400,"Please Enter the name")
     const {tenant, rawKey} = await createTenant(name)
     return res.status(201).json(new ApiResponse(201,{tenant,rawKey}, "Tenant Created Successfully"))
 })
