5a8965cd009814d51685ae842f7830c9bd1874b0
Priyansh <priyansh00soni@gmail.com>
2026-06-27 00:43:14 +0530

    feat: update createNotificationSchema to include template_id validation

--- src/features/notifications/notification.zodSchema.ts
+++ src/features/notifications/notification.zodSchema.ts
@@ -8,13 +8,13 @@ const rawSchema = z.object({
 })
 
 const templateSchema = z.object({
-    
+    recipient:z.string(),
+    template_id: z.string()
 })
 
-
-export const createNotificationSchema = z.object({
-    channel: z.enum([Channel.EMAIL, Channel.INAPP, Channel.WEBHOOK]),
-    message: z.string().min(1, "Message cannot be empty")
-})
+export const createNotificationSchema = z.union([rawSchema,templateSchema]).refine(
+    (data) => !('template_id' in data && 'message' in data),
+    { message: "Provide either template_id or message, not both" }
+)
 
 export type CreateNotificationInput = z.infer<typeof createNotificationSchema>
