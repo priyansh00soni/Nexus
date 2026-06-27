c01e3f1f1db8c9dc2d787460820a418d16c4edb0
Priyansh <priyansh00soni@gmail.com>
2026-06-27 20:45:57 +0530

    feat: add initial worker setup with database and Redis connections

---
+++ src/worker.ts
@@ -0,0 +1,9 @@
+import connectDB from "./config/db.js";
+import connectRedis from "./config/redis.js";
+
+await connectDB()
+await connectRedis()
+
+await import('./workers/email.worker.js')
+await import('./workers/inapp.worker.js')
+await import('./workers/webhook.worker.js')
