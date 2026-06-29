d96e1de4e26996707fae2b6654875e2edbe6b9a9
Priyansh <priyansh00soni@gmail.com>
2026-06-29 15:30:30 +0530

    feat: add idempotency middleware to handle duplicate requests and caching

--- src/features/notifications/notification.routes.ts
+++ src/features/notifications/notification.routes.ts
@@ -3,8 +3,9 @@ import validate from "../../middleware/validate.middleware.js";
 import authMid from "../../middleware/auth.middleware.js";
 import { createNotificationController } from "./notification.controller.js";
 import { createNotificationSchema } from "./notification.zodSchema.js";
+import idempotencyMid from "../../middleware/idempotency.middleware.js";
 
 const router = Router()
-router.route('/').post(authMid,validate(createNotificationSchema),createNotificationController)
+router.route('/').post(authMid,validate(createNotificationSchema),idempotencyMid,createNotificationController)
 
 export default router
---
+++ src/middleware/idempotency.middleware.ts
@@ -0,0 +1,65 @@
+import type { NextFunction, Request, Response } from "express";
+import asyncHandler from "../utils/asyncHandler.js";
+import crypto from 'crypto'
+import { redis } from "../config/redis.js";
+import { prisma } from "../config/PrismaClient.js";
+import ApiError from "../utils/ApiError.js";
+import ApiResponse from "../utils/ApiResponse.js";
+import logger from "../utils/logger.js";
+
+const idempotencyMid = asyncHandler(async(req:Request,res:Response,next:NextFunction)=>{
+    const idempotencyKey = req.headers['idempotency-key'] as string
+    if(!idempotencyKey) throw new ApiError(400,"Idempotency Key Required.")
+
+    const incomingReqHash = crypto.createHash('sha256').update(JSON.stringify(req.body)).digest('hex')    
+
+    const redisCache = await redis.get(`idempotency:${req.tenant.id}:${idempotencyKey}`)
+
+    if(redisCache){
+        const { requestHash, responseBody } = JSON.parse(redisCache)
+
+    if(requestHash!== incomingReqHash) throw new ApiError(409,"Please use different idempotency key.")
+    
+    if(responseBody) return res.status(200).json(new ApiResponse(201,responseBody,"Notification created Successfully."))
+    }
+        
+    const idempotencyRecord =await prisma.idempotencyRecord.findUnique({
+        where:{idempotency_key_tenant_id:{idempotency_key:idempotencyKey, tenant_id:req.tenant.id}},
+    })
+
+    if(idempotencyRecord){
+        if(incomingReqHash!==idempotencyRecord.request_hash) throw new ApiError(409,"Please use different idempotency key.")
+        
+        if(idempotencyRecord.response_body) return res.status(200).json(new ApiResponse(201,idempotencyRecord.response_body,"Notification created Successfully."))
+    }
+
+    await prisma.idempotencyRecord.create({
+        data:{
+            request_hash:incomingReqHash,
+            status:"PROCESSING",
+            tenant_id:req.tenant.id,
+            idempotency_key:idempotencyKey
+        }
+    })
+
+    const originalJson = res.json.bind(res)
+
+    res.json = (data:any) =>{
+        redis.set(
+            `idempotency:${req.tenant.id}:${idempotencyKey}`,
+            JSON.stringify({requestHash:incomingReqHash, responseBody:data}),
+            'EX',86400
+        ).catch(err => logger.error("Redis cache failed", {error:err.message}))
+
+        prisma.idempotencyRecord.update({
+            where:{idempotency_key_tenant_id:{idempotency_key:idempotencyKey, tenant_id:req.tenant.id}},
+            data:{status:"COMPLETED", response_body:data}
+        }).catch(err => logger.error("DB update failed", { error: err.message }))
+
+        return originalJson(data)
+    }
+    
+    next()
+})
+
+export default idempotencyMid
