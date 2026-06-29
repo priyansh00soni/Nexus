41c084e916b1092667346f1324ffd47ef54b14b8
Priyansh <priyansh00soni@gmail.com>
2026-06-29 22:26:50 +0530

    feat: implement rate limiting middleware for tenant-based request management

---
+++ src/middleware/rateLimit.middleware.ts
@@ -0,0 +1,25 @@
+import { type NextFunction, type Request, type Response } from "express";
+import { redis } from "../config/redis.js";
+import asyncHandler from "../utils/asyncHandler.js";
+import ApiError from "../utils/ApiError.js";
+
+const rateLimit = asyncHandler(async(req:Request,res:Response,next:NextFunction)=>{
+    
+    //works for per tenant
+    const key = `rateLimit:${req.tenant.id}`
+    const now =Date.now()
+    const windowStart = now - 60000
+
+    await redis.zremrangebyscore(key, 0, windowStart) // This deletes every entry in the sorted set whose score, which is the timestamp, is between 0 and windowStart. In simple words, delete everything older than 60 seconds ago. This keeps the sorted set clean and only containing recent requests.
+
+    const leftEntriesCount = await redis.zcard(key) //zcard simply counts how many items currently exist in the sorted set
+
+    if(leftEntriesCount>=100) throw new ApiError(429,"Too many requests")
+    
+    redis.zadd(key,now,`${now}-${Math.random()}`)
+
+    await redis.expire(key, 60)//This sets the sorted set itself to automatically delete after 60 seconds of no activity, just as a safety cleanup so Redis doesn't keep empty or abandoned keys forever for tenants who stop sending requests.
+    next()
+})
+
+export default rateLimit
