1b3517133a927c457337c19f26b32919ed7ef2a7
Priyansh <priyansh00soni@gmail.com>
2026-07-03 01:35:15 +0530

    fix: update validation middleware to include request parameters in schema validation

--- src/middleware/validate.middleware.ts
+++ src/middleware/validate.middleware.ts
@@ -4,7 +4,7 @@ import { type ZodSchema } from "zod";
 import ApiError from "../utils/ApiError.js";
 
 const validate = (schema: ZodSchema) => asyncHandler(async(req:Request,res:Response, next:NextFunction) =>{
-    const result = schema.safeParse(req.body)
+    const result = schema.safeParse({...req.body, ...req.params})
     if(!result.success) throw new ApiError(400, "Validation failed", result.error.flatten().fieldErrors)
     req.body = result.data
     next()
