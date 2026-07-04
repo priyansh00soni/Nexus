1a19e00ef8ba0fb80d6ff6aa18c7b9ebd3d6e627
Priyansh <priyansh00soni@gmail.com>
2026-07-05 01:13:18 +0530

    fix: ensure next middleware is called in correlationID middleware

--- src/middleware/correlationID.middleware.ts
+++ src/middleware/correlationID.middleware.ts
@@ -3,5 +3,6 @@ import type { NextFunction, Request, Response } from "express";
 export const correlationID  = (async(req:Request, res:Response, next:NextFunction)=>{
     const id = crypto.randomUUID()
     req.correlationId = id
+    next()
 })
 
