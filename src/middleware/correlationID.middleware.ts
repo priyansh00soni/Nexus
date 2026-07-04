f2b70259b4d46432c52306f234bafb7c90b3e629
Priyansh <priyansh00soni@gmail.com>
2026-07-04 22:08:07 +0530

    feat: add correlationId to Express Request interface and implement correlationID middleware

---
+++ src/middleware/correlationID.middleware.ts
@@ -0,0 +1,8 @@
+import type { NextFunction, Request, Response } from "express";
+
+export const correlationID  = (async(req:Request, res:Response, next:NextFunction)=>{
+    const id = crypto.randomUUID()
+    req.correlationId = id
+    next()
+})
+
