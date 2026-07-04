d9ff97faff55cd138cc9aa10308114acb87beb24
Priyansh <priyansh00soni@gmail.com>
2026-07-04 18:50:20 +0530

    feat: add worker-specific environment variables and implement metrics endpoint

--- src/worker.ts
+++ src/worker.ts
@@ -1,5 +1,9 @@
 import connectDB from "./config/db.js";
 import connectRedis from "./config/redis.js";
+import express from "express"; 
+import type { Request, Response } from "express";
+import register from "./monitoring/metrics.js";
+import logger from "./utils/logger.js";
 
 await connectDB();
 await connectRedis();
@@ -8,5 +12,13 @@ await import('./workers/email.worker.js');
 await import('./workers/inapp.worker.js');
 await import('./workers/webhook.worker.js');
 
+const app = express(); 
 
+app.get('/metrics',async(req:Request,res:Response)=>{
+  res.set("Content-Type", register.contentType);
+  res.send(await register.metrics());
+})
 
+app.listen(9101,()=>{
+  logger.info(`Worker ${process.env.WORKER_ID} running`)
+})
