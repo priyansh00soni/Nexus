4c8bfb36914dcc17ac1fdaa0a7aaf059802ad592
Priyansh <priyansh00soni@gmail.com>
2026-07-05 00:34:54 +0530

    feat: implement correlation ID middleware and logger integration

--- src/middleware/correlationID.middleware.ts
+++ src/middleware/correlationID.middleware.ts
@@ -1,8 +1,9 @@
 import type { NextFunction, Request, Response } from "express";
+import { storage } from "../utils/asyncStorage.js";
 
 export const correlationID  = (async(req:Request, res:Response, next:NextFunction)=>{
     const id = crypto.randomUUID()
     req.correlationId = id
-    next()
+    storage.run({ correlationId: id }, next);
 })
 
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,14 +1,22 @@
 import winston from "winston";
+import { storage } from "./asyncStorage.js";
+
 
 const logger = winston.createLogger({
     level:"info",
     format: winston.format.combine(
+        winston.format(info=>{
+            const store = storage.getStore()
+            if(store?.correlationId) info.correlationId= store.correlationId
+            return info
+        })(),
         winston.format.timestamp(),  
-        winston.format.json()         
+        winston.format.json(),    
     ),
     transports: [
         new winston.transports.Console() 
     ]
 })
 
-export default logger
+
+export default logger 
