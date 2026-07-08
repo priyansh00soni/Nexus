c0ccef5942dd2ea9816ffb7d70e6dd4c522b3875
Priyansh <priyansh00soni@gmail.com>
2026-07-08 20:16:35 +0530

    fix: add rate limiting middleware to notification creation route

--- src/features/notifications/notification.routes.ts
+++ src/features/notifications/notification.routes.ts
@@ -4,9 +4,10 @@ import authMid from "../../middleware/auth.middleware.js";
 import { createNotificationController, getNotificationsController, getNotificationStatusController } from "./notification.controller.js";
 import { createNotificationSchema, getNotificationsSchema, getNotificationStatusSchema } from "./notification.zodSchema.js";
 import idempotencyMid from "../../middleware/idempotency.middleware.js";
+import rateLimit from "../../middleware/rateLimit.middleware.js";
 
 const router = Router()
-router.route('/').post(authMid,validate(createNotificationSchema),idempotencyMid,createNotificationController)
+router.route('/').post(authMid,validate(createNotificationSchema),idempotencyMid,rateLimit,createNotificationController)
 .get(authMid,validate(getNotificationsSchema),getNotificationsController)
 
 router.route('/:notification_id').get(authMid,validate(getNotificationStatusSchema),getNotificationStatusController)
