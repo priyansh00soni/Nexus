922a5a08ffef53d60f9b13e58ee1e00b6e81bd71
Priyansh <priyansh00soni@gmail.com>
2026-07-11 22:52:39 +0530

    feat: adjust load test parameters and add idempotency key for notifications

--- k6/load-test.js
+++ k6/load-test.js
@@ -1,8 +1,8 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 export const options = {
-  vus: 50,
-  duration: '30s',
+  vus: 1,
+  duration: '5s',
     thresholds: {
     http_req_duration: ['p(95)<500'], 
     http_req_failed: ['rate<0.01'],  
@@ -11,7 +11,7 @@ export const options = {
 
 export default function () {
     
-    const res = http.post('https://nexus-api-ci20.onrender.com/api/v1/notifications', 
+    const res = http.post('https://nexus-api-ci20.onrender.com/api/v1/notification',
     JSON.stringify({
       recipient: 'test@example.com',
       channel: 'EMAIL',
@@ -20,10 +20,16 @@ export default function () {
     }),
     { headers: {
         'Content-Type': 'application/json',
-        'x-api-key': 'c05f35e13954f39d9e8bf62cd040144c0cf721033b22b3e3a74e0c6a0dc90a29' } 
-        }
+        'x-api-key': 'c05f35e13954f39d9e8bf62cd040144c0cf721033b22b3e3a74e0c6a0dc90a29',
+        'idempotency-key':Math.random().toString(36).substring(2)
+      },
+      timeout: '30s'
+    }
+         
   )
   
+  console.log(res.status, res.body)
+
   check(res, {
     'status is 201': (r) => r.status === 201,
   })
