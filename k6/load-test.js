2d7c2e17d719eece3e3f3d3958e6787484938caa
Priyansh <priyansh00soni@gmail.com>
2026-07-11 22:53:04 +0530

    feat: increase load test parameters for improved performance testing

--- k6/load-test.js
+++ k6/load-test.js
@@ -1,11 +1,12 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
+
 export const options = {
-  vus: 1,
-  duration: '5s',
-    thresholds: {
-    http_req_duration: ['p(95)<500'], 
-    http_req_failed: ['rate<0.01'],  
+  vus: 50,
+  duration: '30s',
+  thresholds: {
+    http_req_duration: ['p(95)<500'],
+    http_req_failed: ['rate<0.01'],
   }
 }
 
@@ -28,7 +29,6 @@ export default function () {
          
   )
   
-  console.log(res.status, res.body)
 
   check(res, {
     'status is 201': (r) => r.status === 201,
