8926f60a5497627103bbe92fbedd1db371fb8c9b
Priyansh <priyansh00soni@gmail.com>
2026-07-12 12:02:46 +0530

    feat: add OpenAPI documentation for tenant creation endpoint and remove unused type files

--- src/features/tenants/tenant.routes.ts
+++ src/features/tenants/tenant.routes.ts
@@ -5,6 +5,29 @@ import { createTenantSchema } from "./tenant.zodSchema.js";
 
 const router = Router()
 
+
+/**
+ * @openapi
+ * /api/v1/tenant:
+ *   post:
+ *     description: Creates new Tenant.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               from_email:
+ *                  type: string
+ *     responses:
+ *       201:
+ *         description: Returns Tenant's details.
+ */
+
+
 router.route('/').post(validate(createTenantSchema),createTenantController)
 
 export default router
